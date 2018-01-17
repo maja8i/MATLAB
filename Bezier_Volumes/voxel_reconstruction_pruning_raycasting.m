0f15e4eeded0062af55dd6c6caa47094e0ec3563
Maja Krivokuca <maja@8i.com>
2018-01-17 17:47:47 +1300

    -Modified range in which to consider occupied voxels inside a BV (so that it is larger than [0, 1]).
    -Added code inside voxel_reconstruction_pruning_raycasting.m, to remove any reconstructed voxels that may be outside the range [0, (2^b - 1)].

--- Bezier_Volumes/voxel_reconstruction_pruning_raycasting.m
+++ Bezier_Volumes/voxel_reconstruction_pruning_raycasting.m
@@ -41,6 +41,33 @@ for lvl = pp_first_nonempty:size(post_pruning_array, 1)
     pp_leaves_only{lvl} = leaves;
 end
 
+%For debugging purposes, print out how many (if any) of the leaf cells at 
+%each octree level in pp_leaves_only, end up having all 8 of their 
+%decoder-reconstructed control points with the same sign (+/-)
+if debug_flag == 1
+    for lvl = pp_first_nonempty:size(post_pruning_array, 1)
+        same_sign_cntr = 0;
+        zero_cp_cntr = 0;
+        for occ_cell = pp_leaves_only{lvl}'
+            %Get all 8 control points for the corners of the current leaf 
+            %cell
+            current_ctrlpts = reconstruction_decoder{lvl}(ctrl_pts_pointers{lvl}((occ_cell*8 - 7):(occ_cell*8)));
+            %Check if all control points of the current leaf cell have the 
+            %same sign, including the case where all the control points may
+            %be 0
+            if (abs(sum(sign(current_ctrlpts))) == 8)||(~any(sign(current_ctrlpts)))
+                same_sign_cntr = same_sign_cntr + 1;
+                if ~any(sign(current_ctrlpts))
+                    zero_cp_cntr = zero_cp_cntr + 1;
+                end
+            end
+        end %End occ_cell
+        disp(['TOTAL number of leaf cells with all control points having the same sign, at level ' num2str(lvl) ': ' num2str(same_sign_cntr) '/' num2str(length(pp_leaves_only{lvl})) ' (' num2str((same_sign_cntr/(length(pp_leaves_only{lvl}))*100)) '%)']);
+        disp(['No. of leaf cells with all 0 control points at level ' num2str(lvl) ': ' num2str(zero_cp_cntr)]);
+        disp(' ');
+    end %End lvl
+end
+
 %For each octree level at which there are leaf cells, except the voxel
 %level ...
 for lvl = pp_first_nonempty:size(post_pruning_array, 1)
@@ -73,13 +100,9 @@ for lvl = pp_first_nonempty:size(post_pruning_array, 1)
                 %(i.e., is on the face at x = 0, or on the face at x = 1)
                 min_y = min(current_corner_coords(:, 2));
                 max_y = max(current_corner_coords(:, 2));
-%                 y_coords_norm = ((min_y:max_y) - min_y)/BV_side_length;  %Coordinates have to be in [0, 1] for trilinear interpolation formula
-%                 y_coords = y_coords_norm(1:(end - 1)) + 1/(2*BV_side_length);
                 y_coords = ((min_y:(max_y - 1)) - min_y + 0.5)/BV_side_length;
                 min_z = min(current_corner_coords(:, 3));
                 max_z = max(current_corner_coords(:, 3));
-%                 z_coords_norm = ((min_z:max_z) - min_z)/BV_side_length;  %Coordinates have to be in [0, 1] for trilinear interpolation formula
-%                 z_coords = z_coords_norm(1:(end - 1)) + 1/(2*BV_side_length);
                 z_coords = ((min_z:(max_z - 1)) - min_z + 0.5)/BV_side_length;
                 %Solve for x coordinates, to find any occupied voxels in
                 %the current BV ...
@@ -116,18 +139,14 @@ for lvl = pp_first_nonempty:size(post_pruning_array, 1)
                 %Check which of the x_coords are in the range [0, 1]: these
                 %represent occupied voxels on the surface inside the
                 %current BV block
-%                 inds = find((x_coords >= 1/(2*BV_side_length)) & (x_coords <= (1 - 1/(2*BV_side_length))));
-                inds = find((x_coords >= 0) & (x_coords <= 1));
+                %inds = find((x_coords >= 0) & (x_coords <= 1));
+                inds = find((x_coords >= -2.5/BV_side_length) & (x_coords <= (1 + 2.5/BV_side_length)));
                 [inds_r, inds_c] = ind2sub([length(y_coords) length(z_coords)], inds);
                 occ_vox_x_coords = x_coords(inds);
                 occ_vox_y_coords = y_coords(inds_c);
                 occ_vox_z_coords = z_coords(inds_r);
                 %Transform occupied voxels' x, y, z coordinates back from
                 %[0, 1] to their original ranges                
-%                 occ_vox_y_coords = ((occ_vox_y_coords - (1/(2*BV_side_length))).*BV_side_length + min_y + 0.5)';
-%                 occ_vox_z_coords = ((occ_vox_z_coords - (1/(2*BV_side_length))).*BV_side_length + min_z + 0.5)';
-%                 min_x_midpoint = min(current_corner_coords(:, 1)) + 0.5;
-%                 occ_vox_x_coords = (occ_vox_x_coords.*BV_side_length) + min_x_midpoint;
                 occ_vox_y_coords = (occ_vox_y_coords.*BV_side_length + min_y)';
                 occ_vox_z_coords = (occ_vox_z_coords.*BV_side_length + min_z)';
                 occ_vox_x_coords = occ_vox_x_coords.*BV_side_length + min(current_corner_coords(:, 1));
@@ -145,13 +164,9 @@ for lvl = pp_first_nonempty:size(post_pruning_array, 1)
                 %(i.e., is on the face at y = 0, or on the face at y = 1)
                 min_x = min(current_corner_coords(:, 1));
                 max_x = max(current_corner_coords(:, 1));
-%                 x_coords_norm = ((min_x:max_x) - min_x)/BV_side_length;  %Coordinates have to be in [0, 1] for trilinear interpolation formula
-%                 x_coords = x_coords_norm(1:(end - 1)) + 1/(2*BV_side_length);
                 x_coords = ((min_x:(max_x - 1)) - min_x + 0.5)/BV_side_length;
                 min_z = min(current_corner_coords(:, 3));
                 max_z = max(current_corner_coords(:, 3));
-%                 z_coords_norm = ((min_z:max_z) - min_z)/BV_side_length;  %Coordinates have to be in [0, 1] for trilinear interpolation formula
-%                 z_coords = z_coords_norm(1:(end - 1)) + 1/(2*BV_side_length);
                 z_coords = ((min_z:(max_z - 1)) - min_z + 0.5)/BV_side_length;
                 %Solve for y coordinates, to find any occupied voxels in
                 %the current BV ...
@@ -188,18 +203,14 @@ for lvl = pp_first_nonempty:size(post_pruning_array, 1)
                 %Check which of the y_coords are in the range [0, 1]: these
                 %represent occupied voxels on the surface inside the
                 %current BV block
-%                 inds = find((y_coords >= 1/(2*BV_side_length)) & (y_coords <= (1 - 1/(2*BV_side_length))));
-                inds = find((y_coords >= 0) & (y_coords <= 1));
+                %inds = find((y_coords >= 0) & (y_coords <= 1));
+                inds = find((y_coords >= -2.5/BV_side_length) & (y_coords <= (1 + 2.5/BV_side_length)));
                 [inds_r, inds_c] = ind2sub([length(x_coords) length(z_coords)], inds);
                 occ_vox_y_coords = y_coords(inds);
                 occ_vox_x_coords = x_coords(inds_c);
                 occ_vox_z_coords = z_coords(inds_r);
                 %Transform occupied voxels' x, y, z coordinates back from
                 %[0, 1] to their original ranges
-%                 occ_vox_x_coords = ((occ_vox_x_coords - (1/(2*BV_side_length))).*BV_side_length + min_x + 0.5)';
-%                 occ_vox_z_coords = ((occ_vox_z_coords - (1/(2*BV_side_length))).*BV_side_length + min_z + 0.5)';
-%                 min_y_midpoint = min(current_corner_coords(:, 2)) + 0.5;
-%                 occ_vox_y_coords = (occ_vox_y_coords.*BV_side_length) + min_y_midpoint;
                 occ_vox_x_coords = (occ_vox_x_coords.*BV_side_length + min_x)';
                 occ_vox_z_coords = (occ_vox_z_coords.*BV_side_length + min_z)';
                 occ_vox_y_coords = occ_vox_y_coords.*BV_side_length + min(current_corner_coords(:, 2));
@@ -217,13 +228,9 @@ for lvl = pp_first_nonempty:size(post_pruning_array, 1)
                 %(i.e., is on the face at z = 0, or on the face at z = 1)
                 min_x = min(current_corner_coords(:, 1));
                 max_x = max(current_corner_coords(:, 1));
-%                 x_coords_norm = ((min_x:max_x) - min_x)/BV_side_length;  %Coordinates have to be in [0, 1] for trilinear interpolation formula
-%                 x_coords = x_coords_norm(1:(end - 1)) + 1/(2*BV_side_length);
                 x_coords = ((min_x:(max_x - 1)) - min_x + 0.5)/BV_side_length;
                 min_y = min(current_corner_coords(:, 2));
                 max_y = max(current_corner_coords(:, 2));
-%                 y_coords_norm = ((min_y:max_y) - min_y)/BV_side_length;  %Coordinates have to be in [0, 1] for trilinear interpolation formula 
-%                 y_coords = y_coords_norm(1:(end - 1)) + 1/(2*BV_side_length);
                 y_coords = ((min_y:(max_y - 1)) - min_y + 0.5)/BV_side_length;
                 %Solve for z coordinates, to find any occupied voxels in
                 %the current BV ...
@@ -260,18 +267,14 @@ for lvl = pp_first_nonempty:size(post_pruning_array, 1)
                 %Check which of the z_coords are in the range [0, 1]: these
                 %represent occupied voxels on the surface inside the
                 %current BV block
-%                 inds = find((z_coords >= 1/(2*BV_side_length)) & (z_coords <= (1 - 1/(2*BV_side_length))));
-                inds = find((z_coords >= 0) & (z_coords <= 1));
+                %inds = find((z_coords >= 0) & (z_coords <= 1));
+                inds = find((z_coords >= -2.5/BV_side_length) & (z_coords <= (1 + 2.5/BV_side_length)));
                 [inds_r, inds_c] = ind2sub([length(x_coords) length(y_coords)], inds);
                 occ_vox_z_coords = z_coords(inds);
                 occ_vox_x_coords = x_coords(inds_c);
                 occ_vox_y_coords = y_coords(inds_r);
                 %Transform occupied voxels' x, y, z coordinates back from
                 %[0, 1] to their original ranges
-%                 occ_vox_x_coords = ((occ_vox_x_coords - (1/(2*BV_side_length))).*BV_side_length + min_x + 0.5)';
-%                 occ_vox_y_coords = ((occ_vox_y_coords - (1/(2*BV_side_length))).*BV_side_length + min_y + 0.5)';
-%                 min_z_midpoint = min(current_corner_coords(:, 3)) + 0.5;
-%                 occ_vox_z_coords = (occ_vox_z_coords.*BV_side_length) + min_z_midpoint;
                 occ_vox_x_coords = (occ_vox_x_coords.*BV_side_length + min_x)';
                 occ_vox_y_coords = (occ_vox_y_coords.*BV_side_length + min_y)';
                 occ_vox_z_coords = occ_vox_z_coords.*BV_side_length + min(current_corner_coords(:, 3));
@@ -289,6 +292,12 @@ end %End lvl
 %If there are any surplus rows in reconstructed_vox_pos (because the
 %initial matrix size was made too large), remove them now
 reconstructed_vox_pos((vox_cntr:end), :) = [];
+
+%Also, remove any voxels that are outside the range [0, (2^b - 1)]
+[rows_to_remove, ~, ~] = find(reconstructed_vox_pos < 0);
+reconstructed_vox_pos(rows_to_remove, :) = [];
+[rows_to_remove2, ~, ~] = find(reconstructed_vox_pos > (2^b - 1));
+reconstructed_vox_pos(rows_to_remove2, :) = [];
 
 
 
